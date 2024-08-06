ba3a9c5dafda0a38ab1855fb49ad28f88b3a8814
Chunghyup Mok <36430411+mokch@users.noreply.github.com>
2024-08-06 13:34:48 +0900

    Update loss.py

--- CAIT/loss.py
+++ CAIT/loss.py
@@ -6,32 +6,6 @@ import torch
 
 device = torch.device('cuda:0')
 
-class InfoNCE_Loss(nn.Module):
-    def __init__(self, temperature):
-        super(InfoNCE_Loss, self).__init__()
-        self.temperature = temperature
-        self.similarity_f = nn.CosineSimilarity(dim=2)
-
-    def forward(self, z_i, z_j):
-
-        z_i = F.normalize(z_i, p=2, dim=1)
-        z_j = F.normalize(z_j, p=2, dim=1)
-
-        N_pos = z_i.shape[0]
-        N_neg = z_j.shape[0]
-        sim_pos = self.similarity_f(z_i.unsqueeze(1), z_i.unsqueeze(0)) / self.temperature
-        sim_neg = self.similarity_f(z_i.unsqueeze(1), z_j.unsqueeze(0)) / self.temperature
-
-        sim_pos = sim_pos.reshape(N_pos*N_pos, -1)
-        mask = torch.zeros(sim_pos.shape[0], sim_pos.shape[1], device=device)
-        # top_sim = torch.where(sim_pos > 0, sim_pos, mask)
-        top_sim = sim_pos
-        pos_exp = torch.exp(top_sim)
-        neg_exp = torch.exp(sim_neg.reshape(N_pos*N_neg, -1))
-
-
-        return -torch.log(pos_exp.sum()/(neg_exp.sum()+pos_exp.sum()))
-    
 class Ratio_Loss(nn.Module):
     def __init__(self):
         super(Ratio_Loss, self).__init__()
