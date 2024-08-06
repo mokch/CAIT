ebbf35c68d8b7cd78da36647495d4f04c4835a8b
Chunghyup Mok <36430411+mokch@users.noreply.github.com>
2024-08-06 13:55:04 +0900

    Update main.py

--- CAIT/main.py
+++ CAIT/main.py
@@ -1,21 +1,31 @@
+import argparse
 from train_model import trainer
 import os 
 
-data_dir = './datasets'
-save_dir = './result'
-data_split_see = 2024
-data_name = 'wine.mat'
 
-def main():
-
-    Trainer = trainer(data_name = data_name, data_path = data_dir, data_split_seed = data_split_seed)
-    Trainer.set_save_param(save_dir = save_dir, loss_plot = True, save_result = True)
-    Trainer.load_dataloader(batch_size = 32)
-    Trainer.set_model(model_version = 'v1', depth = 2, embed_dim = 256, device = 'cuda:0')
-    Trainer.set_train_param(patience = -1, optimizer = 'adam', epochs = 100, num_corruption = 64, model_loss = 'cos_ratio')
+def main(args):
+    Trainer = trainer(data_name=args.data_name, data_path=args.data_dir, data_split_seed=args.data_split_seed)
+    Trainer.set_save_param(save_dir=args.save_dir, loss_plot=True, save_result=True)
+    Trainer.load_dataloader(batch_size = args.batch_size)
+    Trainer.set_model(model_version = 'v1', depth = 2, embed_dim = 256, device = args.device)
+    Trainer.set_train_param(patience = -1, optimizer = 'adam', epochs = args.epochs, num_corruption = args.num_corruption, model_loss = 'cos_ratio')
     Trainer.train()
     Trainer.prepare_test()
     Trainer.test() 
-    
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Train a model with specified parameters")
+    parser.add_argument('--data_name', type=str, default='wine.mat', help='Dataset name')
+    parser.add_argument('--data_dir', type=str, default='./datasets', help='Directory for datasets')
+    parser.add_argument('--save_dir', type=str, default='./result', help='Directory for saving results')
+    parser.add_argument('--data_split_seed', type=int, default=2024, help='Seed for data splitting')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
+    parser.add_argument('--device', type=str, default='cuda', help='Device to use for training (e.g., cuda:0 or cpu)')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs for training')
+    parser.add_argument('--num_corruption', type=int, default=64, help='Number of corruptions for training')
+
+    args = parser.parse_args()
+    main(args)
+
+
+
